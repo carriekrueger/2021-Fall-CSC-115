c26f6eee325ea5edd1c68f3c5663494e2930db4f
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2021-09-29 11:13:37 -0400

    Create IfElseNotes.java

---
+++ IfElseNotes.java
@@ -0,0 +1,53 @@
+// Carrie Krueger
+// 9-29-21
+// Notes on Ifs and Elses
+
+import java.util.Scanner;
+
+public class IfElseNotes {
+   public static void main(String[] args) {
+      ifMethod(); // let's explore ifs and elses
+   }
+   
+   //method to study if statements
+   public static void ifMethod() {
+   
+      // Task: ask the user for a number and tell them if it's even
+      
+      Scanner sc = new Scanner(System.in);  // make a Scanner that reads from the keyboard
+      
+      System.out.println("Enter a number: "); // prompt the user for a number
+      int number = sc.nextInt(); // read and store number 
+      
+      /* Three common mistakes:
+         1. DON'T put a ';' after the if() 
+         2. DO put curly braces {  } around the code to be executed if true
+         3. DO remember to test for equality with '=='
+      */
+      
+      // test for 'evenness' using mod
+      if(number % 2 == 0) {
+         System.out.println(number + " is even.");
+         System.out.println("Rock on!!");
+      }
+      
+      System.out.println("The end.");  // prints every time
+      
+      // Task: compare two numbers 
+      
+      int num1 = 12; 
+      int num2 = 7; 
+      
+      // compare and output message
+      if(num1 < num2)  {
+         System.out.println("num1 is less!");
+      
+      } else {
+         System.out.println("num1 is NOT less!");  // why not num2 less?
+      
+      }
+      
+      sc.close();
+   
+   }
+}
